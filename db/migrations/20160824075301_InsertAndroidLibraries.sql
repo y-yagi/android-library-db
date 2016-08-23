af707746667601760c74cbdd259c1fbfd6d1fef9
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-08-24 08:46:32 +0900

    add libraries

---
+++ db/migrations/20160824075301_InsertAndroidLibraries.sql
@@ -0,0 +1,8 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+INSERT INTO android_libraries (package, release_note_url, created_at, updated_at) VALUES ('com.android.support:support-v13', 'https://developer.android.com/topic/libraries/support-library/revisions.html',  now(), now());
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DELETE FROM android_libraries WHERE package in ('com.android.support:support-v13');
