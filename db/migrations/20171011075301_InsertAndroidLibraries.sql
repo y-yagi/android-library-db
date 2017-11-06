08e127adb6ae7838e5a6f8fa0e2d077d9de639f6
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-11-07 07:09:15 +0900

    Fix file name

--- db/migrations/20171011075301_InsertAndroidLibraries.sql
+++ db/migrations/20171011075301_InsertAndroidLibraries.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-INSERT INTO android_libraries (package, release_note_url, created_at, updated_at) VALUES ('io.sentry:sentry-logback', 'https://github.com/getsentry/sentry-java/blob/master/CHANGES',  now(), now());
+INSERT INTO android_libraries (package, release_note_url, created_at, updated_at) VALUES ('org.antlr:stringtemplate', 'https://github.com/antlr/stringtemplate4/releases',  now(), now());
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
-DELETE FROM android_libraries WHERE package in ('io.sentry:sentry-logback');
+DELETE FROM android_libraries WHERE package in ('org.antlr:stringtemplate');
